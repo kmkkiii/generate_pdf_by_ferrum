f81dc962c7d597a579e50049e446266515405e25
kmkkiii <rskkmkmk@gmail.com>
2023-12-22 12:15:16 +0900

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    member do
+      get :download_pdf
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
